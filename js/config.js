ea8ec6984378211fc7a8ca41b00392b5b0bbd9f5
Mino <mkuc@demant.com>
2026-02-07 14:38:05 +0100

    Allow file protocol to trigger dev mode for testing button

--- js/config.js
+++ js/config.js
@@ -25,7 +25,8 @@ const Config = (function () {
      */
     function isDevMode() {
         return window.location.hostname === 'localhost' ||
-            window.location.hostname === '127.0.0.1';
+            window.location.hostname === '127.0.0.1' ||
+            window.location.protocol === 'file:';
     }
 
     /**
